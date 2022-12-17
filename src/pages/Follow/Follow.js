89080bc4e1c04c939f092b82d6e2f6cda3b80978
vinh <vinhphamdev0406@gmail.com>
2022-12-17 08:51:38 +0700

    update

--- src/pages/Follow/Follow.js
+++ src/pages/Follow/Follow.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Loading from "~/components/loading/Loading";
 import WrapperLayout from "~/components/wrapperLayout";
+import { setLoading } from "~/redux-toolkit/global/globalSlice";
 import PostItem from "./PostItem/PostItem";
 
 const Follow = () => {
   const [dataFollow, setDataFollow] = useState([]);
+  const { loading } = useSelector((state) => state.global);
+  const dispatch = useDispatch();
   useEffect(() => {
+    dispatch(setLoading(true));
     async function fetch() {
       try {
         const response = await axios.get(
@@ -14,9 +20,11 @@ const Follow = () => {
         console.log("data follow:", response.data.data);
         if (response.data && response.data.data) {
           setDataFollow(response.data.data);
+          dispatch(setLoading(false));
         }
       } catch (error) {
         console.error(error);
+        dispatch(setLoading(false));
       }
     }
     fetch();
@@ -25,19 +33,22 @@ const Follow = () => {
   }, []);
   return (
     <WrapperLayout>
-      <div className="follow-container mt-12">
-        <div className="grid grid-cols-3 post-list-item gap-y-5 gap-x-7">
-          {dataFollow?.items &&
-            dataFollow.items.map((item, index) => {
-              return (
-                <PostItem
-                  key={`${item.publishTime}${item.title}`}
-                  item={item}
-                />
-              );
-            })}
+      {loading && <Loading />}
+      {!loading && (
+        <div className="follow-container mt-12">
+          <div className="grid grid-cols-3 post-list-item gap-y-5 gap-x-7">
+            {dataFollow?.items &&
+              dataFollow.items.map((item, index) => {
+                return (
+                  <PostItem
+                    key={`${item.publishTime}${item.title}`}
+                    item={item}
+                  />
+                );
+              })}
+          </div>
         </div>
-      </div>
+      )}
     </WrapperLayout>
   );
 };
